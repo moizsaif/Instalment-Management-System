86cc643f1811af6913d6898cd358bcc5ce8cc506
Adil Ashfaq <adilashfaq877@gmail.com>
2018-10-21 21:39:30 +0500

    CRUD of Product by Adil

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index');
 
 Route::resource('PurchaseOrder', 'PurchaseOrderController');
 Route::resource('GRN','GRNController');
+Route::resource('Product','ProductController');
